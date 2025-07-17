72ee561f36451f628e8e43442abbd3bf74c6d950
igorniko-astr <igornikonorov.igor@yandex.ru>
2025-07-17 17:07:10 +0300

    Langmapper config and compat with which-key and snacks

--- lua/plugins/langmapper.lua
+++ lua/plugins/langmapper.lua
@@ -72,12 +72,43 @@ local default_config = {
 }
 
 return {
-  'Wansmer/langmapper.nvim',
-  lazy = false,
-  priority = 1, -- High priority is needed if you will use `autoremap()`
-  config = function()
-    require('langmapper').setup(default_config)
-  end,
+  {
+    'Wansmer/langmapper.nvim',
+    lazy = false,
+    enabled = true,
+    priority = 1, -- High priority is needed if you will use `autoremap()`
+    config = function()
+      require('langmapper').setup(default_config)
+    end,
+    opts = {},
+  },
+  {
+    "folke/which-key.nvim",
+    opts = function(_, opts)
+      local translate_key = require("langmapper.utils").translate_keycode
+      -- don't show mappings translated by langmapper.nvim. Show entry if func returns true
+      opts.filter = function(mapping)
+        return mapping.lhs
+          and mapping.lhs == translate_key(mapping.lhs, "default", "ru")
+          and mapping.desc
+          and mapping.desc:find("LM") == nil
+      end
+    end,
+  },
+  {
+    "folke/snacks.nvim",
+    optional = true,
+    opts = function(_, _)
+      local translate_key = require("langmapper.utils").translate_keycode
+      local normkey_orig = Snacks.util.normkey
+      Snacks.util.normkey = function(key)
+        if key then
+          key = translate_key(key, "default", "ru")
+        end
+        return normkey_orig(key)
+      end
+    end,
+  },
 }
 
 
